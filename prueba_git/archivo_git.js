d59d057f0dd38a30d28aa359edd45608707bd44b
Leonardo <alber01leo@gmail.com>
2024-09-16 15:37:12 -0300

    prueba_de_error_git

--- prueba_git/archivo_git.js
+++ prueba_git/archivo_git.js
@@ -1,3 +1,7 @@
 console.log("prueba branch");
 
 console.log("segunda preuba de branch, comprobracion de commit");
+
+console.log("tercera prueba de branch, comprobacion de commit");
+
+console.log("cuarta prueba de branch, comprobacion de commit");
